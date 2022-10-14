850d9afa272c7d0e0e5873f33ec50b63a9484907
Valentin Afonso <valentinafonso22@gmail.com>
2022-10-14 12:56:03 +0200

    update app.js

--- App.js
+++ App.js
@@ -1,18 +1,18 @@
 import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import MyStack from './MyStack';
-// import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-// const queryClient = new QueryClient();
+const queryClient = new QueryClient();
 
 export default function App() {
 
   return (
-    //<QueryClientProvider client={queryClient}>
-    <NavigationContainer>
-      <MyStack></MyStack>
-    </NavigationContainer>
-    // </QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      <NavigationContainer>
+        <MyStack></MyStack>
+      </NavigationContainer>
+    </QueryClientProvider>
 
 
   );
