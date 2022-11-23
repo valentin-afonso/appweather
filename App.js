5302c9905a53c216d02f87ea8f86e5c7a392ccef
Valentin Afonso <valentinafonso22@gmail.com>
2022-11-23 21:39:46 +0100

    add eslint and fix code style

--- App.js
+++ App.js
@@ -1,27 +1,15 @@
-import { StyleSheet } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import BottomBar from './navigation/BottomBar';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { NavigationContainer } from '@react-navigation/native'
+import BottomBar from './navigation/BottomBar'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
+const queryClient = new QueryClient()
 
-
-const queryClient = new QueryClient();
-
-export default function App() {
-
+export default function App () {
   return (
-    <QueryClientProvider client={queryClient}>
+    <QueryClientProvider client={ queryClient }>
       <NavigationContainer>
-        <BottomBar></BottomBar>
+        <BottomBar/>
       </NavigationContainer>
     </QueryClientProvider>
-  );
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- components/DatasList.js
+++ components/DatasList.js
@@ -1,25 +1,24 @@
-import React, { useState, useCallback } from 'react';
-import theme from '../styles/theme.style';
-import { useQuery } from '@tanstack/react-query';
-import { StyleSheet, Text, View, ScrollView, RefreshControl } from 'react-native';
-import * as api from '../api/citiesApi';
+import React, { useState, useCallback } from 'react'
+import theme from '../styles/theme.style'
+import { useQuery } from '@tanstack/react-query'
+import { StyleSheet, Text, View, ScrollView, RefreshControl } from 'react-native'
+import * as api from '../api/citiesApi'
 
-export default function DatasList({ navigation, valueInput }) {
+export default function DatasList ({ navigation, valueInput }) {
+  const { data, isLoading, isSuccess, isError } = useQuery(['weather'], api.getCities)
 
-    const { data, isLoading, isSuccess, isError } = useQuery(['weather'], api.getCities);
+  const [refreshing, setRefreshing] = useState(false)
 
-    const [refreshing, setRefreshing] = useState(false);
+  const wait = (timeout) => {
+    return new Promise(resolve => setTimeout(resolve, timeout))
+  }
 
-    const wait = (timeout) => {
-        return new Promise(resolve => setTimeout(resolve, timeout));
-    }
+  const onRefresh = useCallback(() => {
+    setRefreshing(true)
+    wait(2000).then(() => setRefreshing(false))
+  }, [])
 
-    const onRefresh = useCallback(() => {
-        setRefreshing(true);
-        wait(2000).then(() => setRefreshing(false));
-    }, []);
-
-    return (
+  return (
         <View style={styles.list}>
             <Text style={styles.listTitle}>Trouvez une ville</Text>
             {isLoading &&
@@ -44,17 +43,16 @@ export default function DatasList({ navigation, valueInput }) {
                     }
                 >
                     {valueInput !== '' &&
-                        data.map((city, id) => (
-                            city.nom.indexOf(valueInput) !== -1 &&
+                        data.map((city, id) => (city.nom.indexOf(valueInput) !== -1 &&
                             <Text
                                 style={styles.city}
                                 key={id}
                                 onPress={() => {
-                                    navigation.navigate('Details', {
-                                        cityname: city.nom,
-                                        code: city.code,
-                                        id: id,
-                                    });
+                                  navigation.navigate('Details', {
+                                    cityname: city.nom,
+                                    code: city.code,
+                                    id
+                                  })
                                 }}
                             >
                                 {city.nom}
@@ -67,68 +65,65 @@ export default function DatasList({ navigation, valueInput }) {
                                 style={styles.city}
                                 key={id}
                                 onPress={() => {
-                                    navigation.navigate('Details', {
-                                        cityname: city.nom,
-                                        code: city.code,
-                                        id: id,
-                                    });
+                                  navigation.navigate('Details', {
+                                    cityname: city.nom,
+                                    code: city.code,
+                                    id
+                                  })
                                 }}
                             >
                                 {city.nom}
                             </Text>
                         ))
                     }
-
-
-
                 </ScrollView>
             }
 
         </View>
-    );
+  )
 }
 
 const styles = StyleSheet.create({
-    list: {
-        alignItems: 'center',
-        width: '95%',
-        marginTop: 20,
-    },
-    scrollview: {
-        width: '100%',
-        borderRadius: 20,
-    },
-    listTitle: {
-        textAlign: 'start',
-        margin: 10,
-        color: "#fff",
-        width: '100%',
-        fontSize: 30,
-        paddingLeft: 10,
-    },
-    city: {
-        padding: 20,
-        backgroundColor: theme.SECONDARY_COLOR,
-        color: '#fff',
-        borderBottomWidth: 6,
-        borderColor: '#fff',
-        marginBottom: 1,
-    },
-    white: {
-        color: '#fff',
-    },
-    containerLoading: {
-        backgroundColor: theme.PRIMARY_COLOR,
-        width: '100%',
-        height: '100%',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    containerError: {
-        backgroundColor: theme.PRIMARY_COLOR,
-        width: '100%',
-        height: '100%',
-        justifyContent: 'center',
-        alignItems: 'center',
-    }
-});
+  list: {
+    alignItems: 'center',
+    width: '95%',
+    marginTop: 20
+  },
+  scrollview: {
+    width: '100%',
+    borderRadius: 20
+  },
+  listTitle: {
+    textAlign: 'start',
+    margin: 10,
+    color: '#fff',
+    width: '100%',
+    fontSize: 30,
+    paddingLeft: 10
+  },
+  city: {
+    padding: 20,
+    backgroundColor: theme.SECONDARY_COLOR,
+    color: '#fff',
+    borderBottomWidth: 6,
+    borderColor: '#fff',
+    marginBottom: 1
+  },
+  white: {
+    color: '#fff'
+  },
+  containerLoading: {
+    backgroundColor: theme.PRIMARY_COLOR,
+    width: '100%',
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  containerError: {
+    backgroundColor: theme.PRIMARY_COLOR,
+    width: '100%',
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
